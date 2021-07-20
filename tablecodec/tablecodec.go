4f183f376a43b178dbdef0b4e339a0692b097730
Joyce <2339348937@qq.com>
2021-07-20 16:35:13 +0800

    feat: project1-part2: TestDecodeIndexKey 和 TestRecordKey

--- tablecodec/tablecodec.go
+++ tablecodec/tablecodec.go
@@ -72,7 +72,32 @@ func EncodeRowKeyWithHandle(tableID int64, handle int64) kv.Key {
 // DecodeRecordKey decodes the key and gets the tableID, handle.
 func DecodeRecordKey(key kv.Key) (tableID int64, handle int64, err error) {
 	/* Your code here */
-	return
+	if len(key) <= prefixLen {
+		return 0, 0, errInvalidRecordKey.GenWithStack("invalid record key - %q", key)
+	}
+
+	k :=key
+	if !hasTablePrefix(k) {
+		return 0,0,errInvalidKey.GenWithStack("invalid key - %q", key)
+	}
+	k=k[tablePrefixLength:]
+	k,tableID,err=codec.DecodeInt(k)
+	if err!=nil{
+		return 0,0,errors.Trace(err)
+	}
+
+	if !hasRecordPrefixSep(k){
+		return 0,0,errInvalidKey.GenWithStack("invalid key - %q", key)
+	}
+	k=k[recordPrefixSepLength:]
+	if len(k)==8{
+		k,handle,err=codec.DecodeInt(k)
+		if err!=nil{
+			return 0,0, errors.Trace(err)
+		}
+		return tableID,handle,nil
+	}
+	return 0,0,errInvalidKey.GenWithStack("invalid key - %q", key)
 }
 
 // appendTableIndexPrefix appends table index prefix  "t[tableID]_i".
@@ -95,7 +120,16 @@ func EncodeIndexSeekKey(tableID int64, idxID int64, encodedValue []byte) kv.Key 
 // DecodeIndexKeyPrefix decodes the key and gets the tableID, indexID, indexValues.
 func DecodeIndexKeyPrefix(key kv.Key) (tableID int64, indexID int64, indexValues []byte, err error) {
 	/* Your code here */
-	return tableID, indexID, indexValues, nil
+	k :=key
+	tableID,indexID,isRecord,err:=DecodeKeyHead(k)
+	if err !=nil{
+		return 0,0,nil,errors.Trace(err)
+	}
+	if isRecord{
+		return 0,0,nil,errInvalidIndexKey.GenWithStack("invalid index key - %q %v", key, err)
+	}
+	indexValues=k[prefixLen+idLen:]
+	return tableID,indexID,indexValues,nil
 }
 
 // DecodeIndexKey decodes the key and gets the tableID, indexID, indexValues.
